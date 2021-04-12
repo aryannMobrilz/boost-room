488d6cef0f5620048b2b99ac6ea3f78fa0278559
Alen Vlahovljak <alen.vlahovljak@evolt.dev>
2021-04-12 20:03:31 +0200

    Fix responsivness

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -7,15 +7,20 @@ import Footer from './Footer/Footer';
 import { Container, RegisterButton } from './Auth.style';
 
 export interface AuthProps {
-  span: number;
+  xs?: number;
+  sm?: number;
+  md?: number;
+  lg?: number;
+  xl?: number;
+  xxl?: number;
   layout?: 'horizontal' | 'inline' | 'vertical' | undefined;
   onFinish: ((values: unknown) => void) | undefined;
   form: FormInstance<unknown>;
 }
 
-const Auth: FC<AuthProps> = ({ children, span, layout, form, onFinish }) => {
+const Auth: FC<AuthProps> = ({ children, layout, xs, sm, md, lg, xl, xxl, form, onFinish }) => {
   return (
-    <Container span={span}>
+    <Container xs={xs} sm={sm} md={md} lg={lg} xl={xl} xxl={xxl}>
       <Header />
       <Form layout={layout} form={form} onFinish={onFinish}>
         {children}
