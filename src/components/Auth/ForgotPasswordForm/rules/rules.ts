f22791320720b8abd00b92946abc6afe8efa4478
Alen Vlahovljak <alen.vlahovljak@evolt.dev>
2021-04-12 20:24:38 +0200

    Fix responsivness

---
+++ src/components/Auth/ForgotPasswordForm/rules/rules.ts
@@ -0,0 +1,5 @@
+export const rules = {
+  email: [{ required: true, message: 'Email is required!' }],
+  password: [{ required: true, message: 'Password is required!' }],
+  confirmPassword: [{ required: true, message: 'Please confirm your password!' }]
+};
