2c5f1b894496e47199236c252a3c604cd46efddf
Alen Vlahovljak <alen.vlahovljak@evolt.dev>
2021-04-27 14:24:03 +0200

    Implement social picker

--- components/UI/SelectSocialContact/SelectSocialContact.tsx
+++ components/UI/SelectSocialContact/SelectSocialContact.tsx
@@ -1,40 +1,60 @@
-import { FC } from 'react';
+import { useState, Fragment, FC } from 'react';
 
 import Image from 'next/image';
 import { Row, Col, Select, Input } from 'antd';
 import { FormItem, ArrowRight, AddButton, CloseButton } from './SelectSocialContact.style';
 
+type Social = { id: number; label: string };
+type Contact = { social: string; username: string };
+
 export interface SelectSocialContactProps {
-  socials: Record<string, string | number>[];
+  socials: Social[];
 }
 
 const SelectSocialContact: FC<SelectSocialContactProps> = ({ socials }) => {
+  const [username, setUsername] = useState<string>('');
+  const [social, setSocial] = useState<string>('');
+  const [options, setOption] = useState<Contact[]>([{ social: 'Discord', username: 'J_smith' }]);
+
+  const addContact = () => {
+    setOption([...options, { social, username }]);
+  };
+
   return (
     <Row>
-      <Col span={10}>
-        <FormItem>
-          <Select placeholder="Choose Contact / Socials">
-            {socials.map(({ id, label }) => (
-              <Select.Option key={id} value={id}>
-                {label}
-              </Select.Option>
-            ))}
-          </Select>
-        </FormItem>
-      </Col>
-      <ArrowRight span={2}>
-        <Image src="/assets/icons/arrow-right.svg" width={8} height={13} />
-      </ArrowRight>
-      <Col span={10}>
-        <FormItem name="address">
-          <Input />
-        </FormItem>
-      </Col>
-      <Col span={1} push={1}>
-        <CloseButton icon={<Image src="/assets/icons/close.svg" width={22} height={22} />} />
-      </Col>
+      {options.map((option) => (
+        <Fragment key={option.social}>
+          <Col span={10}>
+            <FormItem>
+              <Select
+                placeholder="Choose Contact / Socials"
+                value={option.social}
+                onChange={(value) => setSocial(value as string)}>
+                {socials.map(({ id, label }) => (
+                  <Select.Option key={id} value={label}>
+                    {label}
+                  </Select.Option>
+                ))}
+              </Select>
+            </FormItem>
+          </Col>
+          <ArrowRight span={2}>
+            <Image src="/assets/icons/arrow-right.svg" width={8} height={13} />
+          </ArrowRight>
+          <Col span={10}>
+            <FormItem name={option.social}>
+              <Input defaultValue={option.username} onChange={(e) => setUsername(e.target.value)} />
+            </FormItem>
+          </Col>
+          <Col span={1} push={1}>
+            <CloseButton icon={<Image src="/assets/icons/close.svg" width={22} height={22} />} />
+          </Col>
+        </Fragment>
+      ))}
       <Col span={24}>
-        <AddButton icon={<Image src="/assets/icons/add.svg" width={25} height={25} />}>
+        <AddButton
+          icon={<Image src="/assets/icons/add.svg" width={25} height={25} />}
+          onClick={addContact}>
           Add Contact Method
         </AddButton>
       </Col>
