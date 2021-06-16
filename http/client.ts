669d664a6df0cb8eabe6d36890a0d43c6aa57677
Alen Vlahovljak <alen.vlahovljak@evolt.dev>
2021-06-16 21:54:36 +0200

    configure client api

--- http/client.ts
+++ http/client.ts
@@ -1,65 +1,40 @@
-import { NextPageContext } from 'next';
+import getConfig from 'next/config';
 import axios, { AxiosInstance } from 'axios';
-import cookies from 'next-cookies';
+import axiosBetterStacktrace from 'axios-better-stacktrace';
+
+const { publicRuntimeConfig } = getConfig();
 
 let apiClient: AxiosInstance;
 
-export const baseURL = 'http://localhost:3000/api';
-
-export const createApiClient = (): AxiosInstance =>
-  axios.create({
-    baseURL,
-    withCredentials: true,
-    xsrfCookieName: 'csrftoken',
-    xsrfHeaderName: 'X-CSRFToken'
+function createApiClient() {
+  const client = axios.create({
+    baseURL: publicRuntimeConfig.backendApiHost
   });
+  axiosBetterStacktrace(client);
 
-export function getApiClient(ctx: NextPageContext | undefined): AxiosInstance {
-  if (ctx && ctx.req) {
-    // eslint-disable-next-line no-shadow,@typescript-eslint/no-shadow
-    const apiClient = createApiClient();
-    apiClient.interceptors.request.use(
-      (request) => {
-        request.headers.Cookie = ctx.req?.headers.cookie;
-        const csrfCookie = cookies(ctx).csrftoken;
-        if (csrfCookie) {
-          request.headers['X-CSRFToken'] = csrfCookie;
-        }
-        return request;
-      },
-      (error) => {
-        log(error);
-        throw error;
-      }
-    );
-    apiClient.interceptors.response.use(
-      (response) => {
-        if (response.headers['Set-Cookie']) {
-          ctx.res?.setHeader('Set-Cookie', response.headers['Set-Cookie']);
-        }
-        return response;
-      },
-      (error) => {
-        log(`HTTP Failed: ${error.request.method} ${error.request.path}`);
-        if (error.response) {
-          log(error.response.data);
-        } else {
-          log(error);
-        }
-        throw error;
-      }
-    );
-    return apiClient;
-  }
-  if (!apiClient) {
-    apiClient = createApiClient();
-  }
-  return apiClient;
+  return client;
 }
 
-function log(message: string) {
-  if (process.env.NODE_ENV !== 'production') {
-    // eslint-disable-next-line no-console
-    console.log(message);
-  }
+export function getApiClient(): AxiosInstance {
+  apiClient = createApiClient();
+
+  apiClient.interceptors.request.use(
+    async (config) => {
+      // const token = await getCookie(PAYOUTS_TOKEN);
+
+      config.headers = {
+        // Authorization: `Bearer ${token}`,
+        // Accept: '*/*',
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      };
+
+      return config;
+    },
+    (error) => {
+      Promise.reject(error);
+    }
+  );
+
+  return apiClient;
 }
