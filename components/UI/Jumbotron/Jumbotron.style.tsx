28079928892da2ad74bd3260b6e2fc3f5baecaf3
Alen Vlahovljak <alen.vlahovljak@evolt.dev>
2021-06-24 15:41:28 +0200

    fix jumbotron responsivness

--- components/UI/Jumbotron/Jumbotron.style.tsx
+++ components/UI/Jumbotron/Jumbotron.style.tsx
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled';
-
 import { Row, Button as AntButton, Typography } from 'antd';
+
+import { mediaQuery } from '@/utils/style';
 
 type ContainerProps = {
   src?: string;
@@ -15,32 +16,89 @@ type TextProps = {
 };
 
 export const Container = styled(Row)`
-  padding: 60px 0;
+  padding: 40px 0;
   background: ${({ src }: ContainerProps) => (src ? `url(${src})` : 'var(--tertiary)')};
+
+  ${mediaQuery.sm`
+    padding: 45px 0;
+  `}
+
+  ${mediaQuery.md`
+    padding: 50px 0;
+  `}
+
+  ${mediaQuery.lg`
+    padding: 55px 0;
+  `}
+
+  ${mediaQuery.xl`
+    padding: 60px 0;
+  `}
+`;
+
+export const TextBlock = styled.div`
+  max-width: 1300px;
+  padding: 0 20px;
+  margin: 0 auto;
+
+  ${mediaQuery.md`
+    padding: 0 40px;
+  `}
+
+  ${mediaQuery.xl`
+    padding: 0 50px;
+  `}
 `;
 
 export const Title = styled(Typography.Title)`
   max-width: 700px;
   text-transform: uppercase;
-  font-size: 50px !important;
+  font-size: 30px !important;
   font-weight: bold !important;
   color: var(--white) !important;
   margin: ${({ centered }: TitleProps) => (centered ? '0 auto 5px auto' : '0 0 5px 0 !important')};
   text-align: ${({ centered }: TitleProps) => (centered ? 'center' : 'left')};
-`;
 
-export const TextBlock = styled.div`
-  max-width: 1200px;
-  margin: 0 auto;
+  ${mediaQuery.sm`
+    font-size: 35px !important;
+  `}
+
+  ${mediaQuery.md`
+    font-size: 40px !important;
+  `}
+
+  ${mediaQuery.lg`
+    font-size: 45px !important;
+  `}
+
+  ${mediaQuery.xl`
+    font-size: 50px !important;
+  `}
 `;
 
 export const Text = styled(Typography.Paragraph)`
   max-width: 700px;
   margin: ${({ centered }: TextProps) => (centered ? '0 auto' : '0')};
   text-align: ${({ centered }: TextProps) => (centered ? 'center' : 'left')};
-  font-size: 20px;
+  font-size: 15px;
   font-weight: 500;
   color: var(--white);
+
+  ${mediaQuery.sm`
+    font-size: 16px;
+  `}
+
+  ${mediaQuery.md`
+    font-size: 17px;
+  `}
+
+  ${mediaQuery.lg`
+    font-size: 18px;
+  `}
+
+  ${mediaQuery.xl`
+    font-size: 20px;
+  `}
 `;
 
 export const Button = styled(AntButton)`
@@ -52,6 +110,24 @@ export const Button = styled(AntButton)`
   font-size: 18px;
   height: auto;
   color: var(--white);
-  padding: 15px 60px;
+  padding: 10px 40px;
   border-radius: 3px;
+
+  ${mediaQuery.sm`
+    font-size: 15px;
+  `}
+
+  ${mediaQuery.md`
+    font-size: 16px;
+    padding: 12px 50px;
+  `}
+
+  ${mediaQuery.lg`
+    font-size: 17px;
+  `}
+
+  ${mediaQuery.xl`
+    font-size: 18px;
+    padding: 15px 60px;
+  `}
 `;
