b1516463310d8899fcd7911057f9c344d1ac00f5
Alen Vlahovljak <alen.vlahovljak@evolt.dev>
2021-06-24 17:29:25 +0200

    fix privacy policy title

--- components/PrivacyPolicy/PrivacyPolicy.style.tsx
+++ components/PrivacyPolicy/PrivacyPolicy.style.tsx
@@ -1,9 +1,27 @@
 import styled from '@emotion/styled';
 import { Typography } from 'antd';
 
+import { mediaQuery } from '@/utils/style';
+
 export const Title = styled(Typography.Title)`
-  font-size: 40px !important;
+  font-size: 30px !important;
   font-weight: bold !important;
   color: var(--text-tertiary) !important;
   margin: 0 0 5px 0 !important;
+
+  ${mediaQuery.sm`
+    font-size: 34px !important;
+  `}
+
+  ${mediaQuery.md`
+    font-size: 36px !important;
+  `}
+
+  ${mediaQuery.lg`
+    font-size: 38px !important;
+  `}
+
+  ${mediaQuery.xl`
+    font-size: 40px !important;
+  `}
 `;
