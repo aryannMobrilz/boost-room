9f1e496f611f4dfe6898c438c7a9fbf8fa8ce116
Alen Vlahovljak <alen.vlahovljak@evolt.dev>
2021-06-24 21:08:07 +0200

    Fix contact card responsivness

--- components/layout/containers/ContactUsContainer/ContactUsContainer.style.tsx
+++ components/layout/containers/ContactUsContainer/ContactUsContainer.style.tsx
@@ -7,13 +7,13 @@ export const Container = styled(Row)`
   height: 100%;
   max-width: 900px;
   margin: 0 auto;
-  padding: 30px 30px 40px 30px;
+  padding: 30px 20px;
 
   ${mediaQuery.md`
-    padding: 40px 40px 50px 40px;
+    padding: 50px 40px;
   `}
 
   ${mediaQuery.xl`
-    padding: 50px 50px 60px 50px;
+    padding: 60px 50px;
   `}
 `;
